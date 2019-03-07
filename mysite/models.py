2b5f6ebba6bd88ba9a15fcf53bc9423f798aa2fe
Firoj Shaikh <firojshaikh980.com>
2019-03-07 13:02:26 +0530

    Final commit

---
+++ mysite/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    email = models.EmailField()
+    subject = models.CharField(max_length=196)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
+
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=196)
+    lastname = models.CharField(max_length=196)
+
+
+    def __str__(self):
+        return self.firstname
